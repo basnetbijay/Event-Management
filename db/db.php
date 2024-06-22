f56306662eac2da2759135353bf94c5bc77bca44
shwetaTh <shwetaTh@users.noreply.github.com>
2024-06-22 22:46:21 +0545

    connected to database successful

---
+++ db/db.php
@@ -0,0 +1,12 @@
+<?php
+require '../config/config.php';
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
