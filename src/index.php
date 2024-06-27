d2744b87afe4d8ef59e85dbe6f674d110a2ec54a
shwetaTh <shwetaTh@users.noreply.github.com>
2024-06-27 22:50:37 +0545

    added category list to home and made header sticky

--- src/index.php
+++ src/index.php
@@ -3,23 +3,36 @@ require '../db/db.php';
 require '../includes/header.php';
 require '../utils/auth.php';
 session_start();
-print_r($_SESSION) ;
-if ($_SESSION['is_logged_in']) {
-    echo 'you are logged in';
-}else{
-    echo 'you are not logged in';
-}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ThriftIt - Home</title>
+    <link rel="stylesheet" href="../includes/styles.css"> 
 </head>
 <body>
-    <h1>hello this is home page</h1>
-    <p>add banner here?</p>
-    <a href="../src/loginsignup/logout.php">Logout</a>
+    <div class="container">
+        <div class="category-list">
+            <h3>Categories</h3>
+            <ul>
+                <?php
+                $categories = ['Tops', 'Shoes', 'Pants', 'Bags', 'Skirts', 'Men Shirt', 'Accessories'];
+                foreach ($categories as $category) {
+                    echo '<li><a href="#">';
+                    echo htmlspecialchars($category);
+                    echo '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Welcome to ThriftIt</h1>
+            <p>Add banner here?</p>
+            <a href="../src/loginsignup/logout.php">Logout</a>
+        </div>
+    </div>
 </body>
 </html>
